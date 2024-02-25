6987d16c038aa9a087f1500353fb0bf0806757bc
Diego Ramos <alexisdieram@gmail.com>
2024-02-25 12:15:41 -0600

    Se agregaron entornos de trabajo

--- servertest.js
+++ servertest.js
@@ -3,7 +3,8 @@ const app = require('./app');
 const fs = require('fs');
 require('dotenv').config();
 
-const puerto = process.env.PROD_PORT;
+const puerto = process.env.DEV_PORT;
+
 
 const server =  app.listen(puerto, ()=>{
     console.log('Servidor disponible en https://localhost:'+puerto)
