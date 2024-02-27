673a7bd01eb8620fdb6500338844e0f9a71c4f42
DiegoRT10 <alexisdieram@gmail.com>
2024-02-27 14:00:22 -0600

    Se modifico el icicio

--- api/routes/inicio.js
+++ api/routes/inicio.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 router.get('/',(reg, res) => {
     console.log('entre a la ruta ');
-    res.send('¡Holaaaa!');
+    res.send('Api AUTENTIFICADOR - BALANCEADOR');
 });
 
   
